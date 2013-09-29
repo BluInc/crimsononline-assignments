2940c14a5e73a8c45823d185e2fb6ff45bd01159
Bowen Lu <bluinc@ubuntu.(none)>
2013-09-29 16:10:27 -0700

    final

--- assignment1/question2.py
+++ assignment1/question2.py
@@ -21,7 +21,46 @@ class Article:
             - modify load to load info about related picture (if it exists)
             - modify show to also show the related picture (if it exist)
     '''
-    pass
+    import json
+
+    headline = ''
+    content = ''
+    creator = ''
+    related_image = None
+
+    def __init__(self,head,cont,creat,img):
+        self.headline = head
+        self.content = cont
+        self.creator = creat
+        self.related_image = img
+
+    def show(self):
+        print 'headline: ' + self.headline
+        print 'content: ' + self.content
+        print 'creator: ' + self.creator
+        self.related_image.show()
+
+
+    def save(self):
+        filename = self.creator + '-' + self.headline
+        if not os.path.isfile(filename):
+            with open(filename, 'w') as outfile:
+                json.dumps({'headline':self.headline,'creator':self.creator,'content':self.content,
+                    'related_image':self.related_image.to_json})
+        else:
+            print 'Please rename headline'
+
+    def load(self,filename):
+        try:
+            with open(filename, 'r') as infile:
+                data = json.loads(infile.read())
+                return self.__init__(data['headline'],data['content'],data['creator'],
+                    json.loads(data['related_image']))
+        except IOError:
+            print 'File does not exist'
+
+
+
 
 class Picture:
     '''
@@ -32,4 +71,20 @@ class Picture:
          Methods
             - show (show image)
     '''
-    pass
+    from PIL import image
+    import json
+
+    image_file = ''
+    title = ''
+    creator = ''
+
+    def __init__(self,img_f, t, c):
+        self.image_file = img_f
+        self.title = t
+        self.creator = c
+
+    def to_json(self):
+        return {'image_file':self.image_file,'title':self.title,'creator':self.creator}
+
+    def show(self):
+        Image.open(self.image_file).show()
--- assignment1/question3.py
+++ assignment1/question3.py
@@ -5,7 +5,7 @@ Question 3 - Just a little feedback for us
 '''
 Question 3a: How long did this assignment take you?
 '''
-time_in_minutes = 0 
+time_in_minutes = 60 
 
 '''
 Question 3b: any feedback for us?
@@ -14,4 +14,7 @@ Question 3b: any feedback for us?
 feedback = '''
     Let us know how things are going so far here.  How are the assignments and
     presentations so far?  Anything in particular you want us to talk about?
+
+    Things are going great! I'm learning a lot from the presentations and the
+    assignments. 
 '''
